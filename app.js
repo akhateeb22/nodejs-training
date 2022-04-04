549b029aa5450c87110b05faa1307ef477155fdd
Abd-allah <akhateeb175@gmail.com>
2022-04-04 14:38:46 +0300

    Finishing Uploading File

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
+const multer = require("multer");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -27,9 +28,40 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    const dateStr = new Date().toISOString().replace(/:/g, "-");
+    cb(null, dateStr + "-" + file.originalname);
+  },
+});
 
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    console.log("Passing Filtering on type..!");
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  multer({
+    dest: "images",
+    storage: fileStorage,
+    fileFilter: fileFilter,
+  }).single("image")
+);
 app.use(express.static(path.join(__dirname, "public")));
+app.use('/images',express.static(path.join(__dirname, "images")));
+
+
 app.use(
   session({
     secret: "my secret",
@@ -73,7 +105,7 @@ app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.redicrect('/500');
+  res.redirect("/500");
 });
 
 mongoose
